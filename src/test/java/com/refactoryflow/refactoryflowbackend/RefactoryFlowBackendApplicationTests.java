6867a76356b8573aeb174de8f9d4e4542f1150fe
Rik de Goede <rik.dgd@gmail.com>
2022-03-22 13:49:56 +0100

    initial commit

---
+++ src/test/java/com/refactoryflow/refactoryflowbackend/RefactoryFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refactoryflow.refactoryflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoryFlowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
