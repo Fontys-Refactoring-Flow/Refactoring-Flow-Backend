a966b88f69a916f6f97a1c1645f9537467811b5b
lucvd <lucvd@LAPTOP-7R52BL44>
2022-04-19 08:58:32 +0200

    :100: getChallengeById functie

--- src/main/java/com/refactoryflow/refactoryflowbackend/Controller/ChallengeController.java
+++ src/main/java/com/refactoryflow/refactoryflowbackend/Controller/ChallengeController.java
@@ -3,7 +3,9 @@ package com.refactoryflow.refactoryflowbackend.Controller;
 import com.refactoryflow.refactoryflowbackend.Model.Challenge;
 import com.refactoryflow.refactoryflowbackend.Repository.ChallengeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.refactoryflow.refactoryflowbackend.Exception.ResourceNotFoundException;
 
 import java.util.List;
 
@@ -18,5 +20,11 @@ public class ChallengeController {
     @GetMapping(value = "/challenge")
     public List<Challenge> getAllChallenges(){
         return challengeRepository.findAll();
+    }
+
+    @GetMapping(value = "/challenge/{challengeid}")
+    public Challenge getChallengeById(@PathVariable long challengeid){
+        return challengeRepository.findById(challengeid)
+                .orElseThrow(() -> new ResourceNotFoundException("Challenge not exist with id :" + challengeid));
     }
 }
