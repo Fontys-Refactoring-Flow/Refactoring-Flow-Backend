54234816877905be3eee48aea4efb022be756db9
Rik de Goede <rik.dgd@gmail.com>
2022-05-10 14:52:32 +0200

    Added new approach to file

--- src/main/java/com/refactoryflow/refactoryflowbackend/Controller/CodefileController.java
+++ src/main/java/com/refactoryflow/refactoryflowbackend/Controller/CodefileController.java
@@ -1,9 +1,15 @@
 package com.refactoryflow.refactoryflowbackend.Controller;
 
 import com.refactoryflow.refactoryflowbackend.Model.CodeFile;
-import com.refactoryflow.refactoryflowbackend.Repository.CodeFileRepository;
+import com.refactoryflow.refactoryflowbackend.ResponseMessage;
+import com.refactoryflow.refactoryflowbackend.Service.CodeFileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
@@ -12,16 +18,43 @@ import java.util.List;
 @RequestMapping("api/v1")
 public class CodefileController {
 
+    private CodeFileService codeFileService;
+
     @Autowired
-    private CodeFileRepository codeFileRepository;
+    public CodefileController(CodeFileService codeFileService) {
+        this.codeFileService = codeFileService;
+    }
+
 
     @GetMapping("/CodeFile")
     public List<CodeFile> getCodeFile(){
-        return codeFileRepository.findAll();
+        return codeFileService.getAllCodeFiles();
     }
 
-    @PostMapping("/CodeFile")
-    public CodeFile createCodeFile(@RequestBody CodeFile codefile){
-        return codeFileRepository.save(codefile);
+    @GetMapping("CodeFile/{id}")
+    public ResponseEntity<byte[]> getFile(@PathVariable long id){
+        CodeFile file = codeFileService.getFile(id);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "filename: " + file.getName())
+                .body(file.getData());
+    }
+
+//    @PostMapping("/CodeFile")
+//    public CodeFile createCodeFile(@RequestBody CodeFile codefile){
+//        return codeFileService.postCodeFile(codefile);
+//    }
+
+    @PostMapping("CodeFile")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file){
+        String message;
+        try{
+            codeFileService.store(file);
+            message = "uploaded file successfully";
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        }
+        catch(Exception e){
+            message = "upload failed";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
     }
 }
--- src/main/java/com/refactoryflow/refactoryflowbackend/Model/CodeFile.java
+++ src/main/java/com/refactoryflow/refactoryflowbackend/Model/CodeFile.java
@@ -1,14 +1,8 @@
 package com.refactoryflow.refactoryflowbackend.Model;
 
-import com.sun.istack.NotNull;
 import lombok.*;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
-import java.io.File;
 
 @Getter
 @Setter
@@ -22,6 +16,15 @@ public class CodeFile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long Id;
 
-    @Column(name = "file", nullable = false)
-    private File file;
+    private String Name;
+    private String Type;
+
+    @Lob
+    private byte[] Data;
+
+    public CodeFile(String name, String type, byte[] data) {
+        Name = name;
+        Type = type;
+        Data = data;
+    }
 }
