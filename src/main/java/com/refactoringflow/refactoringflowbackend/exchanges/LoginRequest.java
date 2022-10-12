07cfd37c34fe2062db98d70f0ceedf5e83e5e33d
IemandDieAbeHeet <abe.vriens@gmail.com>
2022-10-12 10:54:10 +0200

    Login with email instead of username

--- src/main/java/com/refactoringflow/refactoringflowbackend/exchanges/LoginRequest.java
+++ src/main/java/com/refactoringflow/refactoringflowbackend/exchanges/LoginRequest.java
@@ -1,11 +1,11 @@
 package com.refactoringflow.refactoringflowbackend.exchanges;
 
 public class LoginRequest {
-    public String name;
+    public String email;
     public String password;
 
-    public LoginRequest(String name, String password) {
-        this.name = name;
+    public LoginRequest(String email, String password) {
+        this.email = email;
         this.password = password;
     }
 }
