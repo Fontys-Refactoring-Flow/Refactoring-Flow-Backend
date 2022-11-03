a5fe17003d05b76f01e5f05ad49394ba067205db
IemandDieAbeHeet <abe.vriens@gmail.com>
2022-11-03 11:02:10 +0100

    Remove unused course files

--- src/main/java/com/refactoringflow/refactoringflowbackend/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.refactoringflow.refactoringflowbackend.repository;
-
-import com.refactoringflow.refactoringflowbackend.model.course.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
