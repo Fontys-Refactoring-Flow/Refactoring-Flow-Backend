e892ebd0d26d21d4cce369a18cfcd01d9dc814c0
IemandDieAbeHeet <abe.vriens@gmail.com>
2022-11-09 12:31:06 +0100

    Change constructor to name instead of username

--- src/main/java/com/refactoringflow/refactoringflowbackend/model/user/StudentPasswordDTO.java
+++ src/main/java/com/refactoringflow/refactoringflowbackend/model/user/StudentPasswordDTO.java
@@ -3,8 +3,8 @@ package com.refactoringflow.refactoringflowbackend.model.user;
 public class StudentPasswordDTO extends StudentDTO {
     public String password;
 
-    public StudentPasswordDTO(Long id, String username, String email, String password, RoleDTO[] roles, Long semester) {
-        super(id, username, email, roles, semester);
+    public StudentPasswordDTO(Long id, String name, String email, String password, RoleDTO[] roles, Long semester) {
+        super(id, name, email, roles, semester);
         this.password = password;
     }
 }
