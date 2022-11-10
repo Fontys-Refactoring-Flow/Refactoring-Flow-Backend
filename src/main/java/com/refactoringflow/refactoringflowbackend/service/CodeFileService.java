c9ec12c4aa36c03c0b674d7dd8f3bca82abf459e
Antwan <antwansittard@gmail.com>
2022-11-10 20:17:58 +0100

    final submit code with one data base error
    kleine database error die alle id van assignment weg gooid:)

--- src/main/java/com/refactoringflow/refactoringflowbackend/service/CodeFileService.java
+++ src/main/java/com/refactoringflow/refactoringflowbackend/service/CodeFileService.java
@@ -1,6 +1,8 @@
 package com.refactoringflow.refactoringflowbackend.service;
 
+import com.refactoringflow.refactoringflowbackend.model.assignment.Assignment;
 import com.refactoringflow.refactoringflowbackend.model.codefile.CodeFile;
+import com.refactoringflow.refactoringflowbackend.model.user.Student;
 import com.refactoringflow.refactoringflowbackend.repository.CodeFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,21 +11,27 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class CodeFileService {
 
     private final CodeFileRepository codeFileRepository;
+    private final AssignmentService assignmentService;
+    private final StudentService studentService;
 
     @Autowired
-    public CodeFileService(CodeFileRepository codeFileRepository) {
+    public CodeFileService(CodeFileRepository codeFileRepository, AssignmentService assignmentService, StudentService studentService) {
         this.codeFileRepository = codeFileRepository;
+        this.assignmentService = assignmentService;
+        this.studentService = studentService;
     }
 
-    public void store(MultipartFile file) throws IOException {
-        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
-        CodeFile CodeFile = new CodeFile(4,fileName, Objects.requireNonNull(file.getContentType()), file.getBytes());
-        codeFileRepository.save(CodeFile);
+    public void store(String code, int assignmentId, int userId, int version) throws IOException {
+        Optional<Assignment> assignment = assignmentService.findById((long)assignmentId);
+        CodeFile CodeFile = new CodeFile((version + 1),assignment.get().getRefactoringType(),"text/x-java-source", code.getBytes());
+        Optional<Student> student = studentService.findById((long)userId);
+        studentService.setCodeFileByAssignmentID(student.get(), assignment.get(), codeFileRepository.save(CodeFile));
     }
 
     public CodeFile getFile(long id){
