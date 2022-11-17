2373927ccabd1712fc8c8cc7590cbd584f7061cb
Antwan <antwansittard@gmail.com>
2022-11-17 10:30:47 +0100

    body dto add

---
+++ src/main/java/com/refactoringflow/refactoringflowbackend/exchanges/CodeFileRequest.java
@@ -0,0 +1,8 @@
+package com.refactoringflow.refactoringflowbackend.exchanges;
+
+public class CodeFileExchange {
+    public Long assignmentId;
+    public Long userId;
+    public String code;
+    public Integer version;
+}
