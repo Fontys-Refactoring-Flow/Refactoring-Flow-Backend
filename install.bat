59a48156b1ae4039d763606f8e0df2a5d2c91c88
IemandDieAbeHeet <abe.vriens@gmail.com>
2023-01-11 12:56:43 +0100

    Add instruction to add password to application.properties

--- install.bat
+++ install.bat
@@ -4,3 +4,5 @@ keytool -keystore keystore.jks -genkey -keyalg RSA -alias dev
 echo Java keystore verplaatsen...
 move keystore.jks ./src/main/resources/keys/
 echo Java keystore klaar, je kunt nu de applicatie starten.
+echo:
+echo Vergeet niet het wachtwoord wat je net hebt ingevoerd ook in te vullen in het application.properties bestand.
